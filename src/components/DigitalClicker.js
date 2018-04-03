da512a1f8a4b6717c6968bc5270d343c7b6c6fa0
porzingod <jasonlim.ny@gmail.com>
2018-04-03 12:00:56 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,28 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  clickHandler = () => {
+    this.setState({
+      timesClicked: this.state.timesClicked + 1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.clickHandler}>{this.state.timesClicked}</button>
+      </div>
+    )
+  }
+}
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,56 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      "errors": [],
+      "settings": {
+        "bitrate": 8,
+        "video": {
+          "resolution": "1080p"
+        }
+      },
+      "user": null
+    }
+  }
+
+  setBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    }, () => console.log(this.state))
+  }
+
+  setResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          resolution: "720p"
+        }
+      }
+    }, () => console.log(this.state))
+  }
+
+  // this.setState({
+  //   addressInfo: {
+  //     ...this.state.addressInfo,
+  //     city: 'New York City',
+  //   },
+  // });
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.setBitrate}>Set Bitrate</button>
+        <button className="resolution" onClick={this.setResolution}>Set Resolution</button>
+      </div>
+    )
+  }
+}
+
+export default YouTubeDebugger;
